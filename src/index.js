5cd3267f63403415f7f932ebdd81e51fe921bdc8
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-06-08 19:47:24 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import validator from './validator.js';
 
-console.log(validator);
+// -----  numeros en consola y no acmi boton imprime losepta vacio -------//
+let boton = document.getElementById('miBoton');
+
+boton.addEventListener("click",captura);
+
+function captura(){
+    let numeros = document.getElementById('numCard').value;
+    let numCondiciones = numeros.length;
+    if(numCondiciones == 0 && numCondiciones < 9){
+        alert('Ingresa un numero');
+        return false;
+    }
+    if(numCondiciones < 9){
+        alert('Numero incompleto');
+        return false;
+    }
+    validator.isValid();
+}
+
+// ----- el input solo acepta numeros-------//
+numCard.addEventListener("keypress", escribeNum);
+let arr = [];
+
+function escribeNum(e){
+
+    if (!soloNumeros(e)){
+        e.preventDefault();
+    }
+
+    function soloNumeros(e){
+        let key = e.charCode;
+        arr.push(key);
+        
+        if(arr.length < 9 ){
+            console.log(arr);
+        }
+        return key >= 48 && key <= 57;
+    }
+}
+
+/*console.log(validator);*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,14 @@
+
+
 const validator = {
-  // ...
+  isValid: function(){
+    
+    alert("Los numeros que ingresaste son ");
+    //console.log(numeros);
+    //document.getElementById('respValido').style.display = 'block';
+  }
 };
+
+//console.log(validator);
 
 export default validator;
