0ff4722959176252f4cbe20eee884672ef2ba715
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-07-26 11:39:57 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,55 @@
+import validator from './validator.js';
+
+// ----- captura numeros del input y no acepta vacio -------//
+let boton = document.getElementById('miBoton');
+boton.addEventListener("click",captura);
+
+function captura(){
+    const numeros = document.getElementById('numCard').value;
+    let numCondiciones = numeros.length;
+    if(numCondiciones == 0){
+        alert('Ingresa un numero');
+        return false;
+    }
+    if(numCondiciones < 13){
+        alert('Numero incompleto');
+        return false;
+    }
+    const isValido = validator.isValid(numeros);
+    if(isValido){
+        document.getElementById('tituloValido').style.display = 'block';
+        document.getElementById('respValido').style.display = 'block';
+        document.getElementById('reload').style.display = 'block';
+        document.getElementById('miBoton').style.display = 'none';
+
+    }else{
+        document.getElementById('tituloNoValido').style.display = 'block';
+        document.getElementById('respNoValido').style.display = 'block';
+        document.getElementById('reload').style.display = 'block';
+        document.getElementById('miBoton').style.display = 'none';
+    }
+    const ocultar = validator.maskify(numeros);
+    if(ocultar){
+        document.getElementById("numCard").value = ocultar;
+    }
+}
+
+// ----- el input solo acepta numeros-------//
+const numCard = document.getElementById('numCard');
+numCard.addEventListener("keypress", escribeNum);
+function escribeNum(e){
+    if (!soloNumeros(e)){
+        e.preventDefault();
+    }
+    function soloNumeros(e){
+        let key = e.charCode;
+        return key >= 48 && key <= 57;
+    }
+}
+
+// ----- refesca la pagina -------//
+const reload = document.getElementById('reload');
+reload.addEventListener('click', recargar);
+function recargar(){
+    location.reload();
+}
---
+++ validator.js
@@ -0,0 +1,58 @@
+//import { createConfigItem } from "@babel/core";
+
+const validator = {
+  isValid: function validar(n){
+    let digitos = n.toString().split('').map(Number);//cadena, int
+
+    digitos.reverse();//inverso
+    //console.log(digitos);
+
+    /* multiplicando posicion par */
+      digitos = digitos.map((digitos, i) =>{
+      if(i%2 === 1){
+        return digitos*2;
+      }else{
+        return digitos;
+      }
+    });
+
+    //console.log(digitos);
+
+    /* sumando digitos de pares */
+    let digitosPar = digitos.map((digitos) =>{
+      if(digitos > 9){
+        return digitos - 9;
+      }else{
+        return digitos;
+      }
+    });
+
+    /* sumando todos los digitos */
+    const totalSum = digitosPar.reduce((acc, digitosPar) => acc = acc + digitosPar, 0);
+
+    //console.log(digitosPar);
+    //console.log(totalSum);
+    //console.log(n);
+
+    /* valido o invalido */
+    return totalSum % 10 === 0;  
+  },
+
+  maskify: function mostrar(nu){
+   //let numOculto = nu.split('').map((letter, i) => i < nu.length - 4 ? '#' : letter).join('');
+
+   let numOculto = '';
+   for(let i = 0; i < nu.length; i++ ){
+     if(i < nu.length-4){
+      numOculto = numOculto + '#'
+     }else{
+       numOculto = numOculto + nu[i]
+     }
+   }
+   //console.log(numOculto);
+
+   return numOculto;
+  }
+};
+
+export default validator;
